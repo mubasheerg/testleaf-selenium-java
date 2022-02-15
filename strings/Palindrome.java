52cd32415b101d2075497f3537cd7ebe32aa445c
mubasheerg <87425459+mubasheerg@users.noreply.github.com>
2022-02-15 08:18:01 +0530

    Add files via upload

---
+++ strings/Palindrome.java
@@ -0,0 +1,24 @@
+package com.testleaf.strings;
+
+import java.util.Scanner;
+
+public class Palindrome {
+	public static void main(String[] args) {
+
+		String str, reverseStr = "";
+		System.out.println("Enter the String to be verified: ");
+		Scanner sc = new Scanner(System.in);
+		str = sc.nextLine();
+		int strLength = str.length();
+
+		for (int i = strLength - 1; i >= 0; --i) {
+			reverseStr = reverseStr + str.charAt(i);
+		}
+
+		if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
+			System.out.println(str + " is a Palindrome String.");
+		} else {
+			System.out.println(str + " is not a Palindrome String.");
+		}
+	}
+}
