e3ebc451b8dc796c461f6d9897c55c004297a4b3
Mubasheer G <87425459+mubasheerg@users.noreply.github.com>
2022-02-21 21:43:20 +0530

    Add files via upload

---
+++ college/Department.java
@@ -0,0 +1,12 @@
+package org.college;
+
+public class Department extends MyProfile {
+	public void branchName() {
+		System.out.println("Branch is Bachelor of Technology");
+	}
+
+	public void deptName() {
+		System.out.println("Specilization is Information Technology");
+	}
+
+}
